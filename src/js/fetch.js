eaeecf71217253b5ca475e2fc380c85e2103af02
Skorohod Andrew <pmisa376@gmail.com>
2022-08-21 13:50:35 +0300

    fix 11 task

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,12 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const PER_PAGES = 40;
-const KEY = '29221253-dd17a46566e1be23f7ca8ff9b';
+const KEY = '29414226-e56ab89f6667b3612bf4ca3ef';
 
-const fetchPictures = async(searchQuery, page) => {
-    const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${PER_PAGES}&page=${page}`);
-    return response.data; 
-}
-export default fetchPictures; 
-
+const fetchPictures = async (searchQuery, page) => {
+  const response = await axios.get(
+    `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${PER_PAGES}&page=${page}`
+  );
+  return response.data;
+};
+export default fetchPictures;
--- src/js/index.js
+++ src/js/index.js
@@ -1,105 +1,106 @@
 import '../css/styles.css';
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchPictures from './fetch';
 
-
 const refs = {
-input: document.querySelector('input'),
-form: document.querySelector('.search-form'),
-buttonLoad: document.querySelector('.load-more'),
-gallery: document.querySelector('.gallery'),
-alert: document.querySelector('.alert')
-}
-
+  input: document.querySelector('input'),
+  form: document.querySelector('.search-form'),
+  buttonLoad: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+  alert: document.querySelector('.alert'),
+};
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.buttonLoad.addEventListener('click', onLoadMoreBtn);
 
-
 let currentPage = 1;
 let searchName = '';
 refs.buttonLoad.classList.add('ishidden');
-const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250,});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
+function onFormSubmit(e) {
+  e.preventDefault();
+  refs.buttonLoad.classList.add('ishidden');
 
-
-    
-function onFormSubmit(e) {    
-e.preventDefault()
-refs.buttonLoad.classList.add('ishidden');
-
-searchName = e.currentTarget.elements.searchQuery.value.trim();
-if (searchName === 0 ) {
-  return;
-} else {
-clearGalleryList();
-currentPage = 1;
-fetchRequest (searchName, currentPage); 
+  searchName = e.currentTarget.elements.searchQuery.value.trim();
+  if (searchName === 0) {
+    return;
+  } else {
+    clearGalleryList();
+    currentPage = 1;
+    fetchRequest(searchName, currentPage);
+  }
 }
-}
 
-
-
-function onLoadMoreBtn(){
+function onLoadMoreBtn() {
   refs.buttonLoad.classList.add('ishidden');
   currentPage += 1;
   searchName = refs.input.value.trim();
-  fetchRequest(searchName, currentPage); 
+  fetchRequest(searchName, currentPage);
 }
 
-async function fetchRequest (searchQuery, currentPage) {
+async function fetchRequest(searchQuery, currentPage) {
   try {
-    const fetchResult = await fetchPictures(searchQuery, currentPage);  
+    const fetchResult = await fetchPictures(searchQuery, currentPage);
     if (currentPage === 1) {
       Notiflix.Notify.info(`Hooray! We found ${fetchResult.totalHits} images.`);
     }
     filterFetchResult(fetchResult);
-} catch (error) {console.log(error)}
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function filterFetchResult(fetchResult) {
   if (currentPage === Math.ceil(fetchResult.totalHits / 40)) {
-      insertMarkup(fetchResult.hits);  
-      refs.buttonLoad.classList.add('ishidden');
-      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-      smoothScrollToBottomPage();
-      lightbox.refresh();
-      return;
+    insertMarkup(fetchResult.hits);
+    refs.buttonLoad.classList.add('ishidden');
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    smoothScrollToBottomPage();
+    lightbox.refresh();
+    return;
   } else if (fetchResult.total === 0) {
-      refs.buttonLoad.classList.add('ishidden');
-      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");   
-      return;
-  } else { 
-      insertMarkup(fetchResult.hits);  
-      refs.buttonLoad.classList.remove('ishidden');
-      smoothScrollToBottomPage();
-      lightbox.refresh();
-      return;
+    refs.buttonLoad.classList.add('ishidden');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  } else {
+    insertMarkup(fetchResult.hits);
+    refs.buttonLoad.classList.remove('ishidden');
+    smoothScrollToBottomPage();
+    lightbox.refresh();
+    return;
   }
 }
 
-function smoothScrollToBottomPage () {
+function smoothScrollToBottomPage() {
   const galleryRect = refs.gallery.getBoundingClientRect();
   window.scrollBy({
-      top: galleryRect.height,
-      behavior: "smooth",
-  })
+    top: galleryRect.height,
+    behavior: 'smooth',
+  });
 }
 
 function insertMarkup(arrayImages) {
   const result = createList(arrayImages);
- 
+
   refs.gallery.insertAdjacentHTML('beforeend', result);
-
 }
 
-function createList (arrayImages) {
-  return arrayImages.reduce((acc, item) => acc + createMarkup(item), "");
+function createList(arrayImages) {
+  return arrayImages.reduce((acc, item) => acc + createMarkup(item), '');
 }
 
-function  createMarkup (img) {
+function createMarkup(img) {
   return `
   <div class="photo-card">
          <a href="${img.largeImageURL}" class="gallery_link">
@@ -120,11 +121,9 @@ function  createMarkup (img) {
               </p>
         </div>
     </div>
-`}
+`;
+}
 
-
-
-
-  function clearGalleryList () {
-    refs.gallery.innerHTML = "";
-};
+function clearGalleryList() {
+  refs.gallery.innerHTML = '';
+}
